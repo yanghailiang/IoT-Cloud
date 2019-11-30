da824567d3c75b099320e11399d4163336dfe37e
yxuan <www.1477439810@qq.com>
2019-11-30 15:22:09 +0800

    init

---
+++ iot-user/src/main/java/com/bysj/iotuser/IotUserApplication.java
@@ -0,0 +1,13 @@
+package com.bysj.iotuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotUserApplication.class, args);
+    }
+
+}
---
+++ iot-user/src/test/java/com/bysj/iotuser/IotUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bysj.iotuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
